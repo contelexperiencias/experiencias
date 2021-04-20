ed398f4f6bfb115a94517e61d0361f20200a1653
contelexperiencias <80924046+contelexperiencias@users.noreply.github.com>
2021-04-20 11:35:28 -0500

    Update welcome.blade.php

--- experiencias/resources/views/welcome.blade.php
+++ experiencias/resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
         <title>Laravel</title>
 
         <!-- Fonts -->
