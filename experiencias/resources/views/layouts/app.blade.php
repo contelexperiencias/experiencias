f55bdab6e8a295323020c8bd858c7a23263012f8
contelexperiencias <80924046+contelexperiencias@users.noreply.github.com>
2021-04-20 11:48:35 -0500

    agregar campo tipo_usuario Y MIGRAR

--- experiencias/resources/views/layouts/app.blade.php
+++ experiencias/resources/views/layouts/app.blade.php
@@ -8,16 +8,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div id="app">
